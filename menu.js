fcfec01a1a7b7f2391409790de2f136b6a168cb1
SoloEvent <47261527+SoloEvent@users.noreply.github.com>
2025-12-22 03:08:31 -0700

    added locations & details

--- menu.js
+++ menu.js
@@ -1,21 +1,21 @@
 const permanentMarkers = {
     weed: [
-        { x: 2355, y: 3780, label: "Weed Location #1", area: "Missing Location", description: "Placeholder description", images: [] },
+        { x: 2355, y: 3780, label: "Weed Location #1", area: "Fort Zancudo Swamp", description: "Found by Drug Officer During routine drug patrol", images: ["https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image6.png"] },
         { x: 6137, y: 3115, label: "Weed Location #2", area: "Missing Location", description: "Placeholder description", images: [] },
         { x: 2472, y: 3666, label: "Weed Location #3", area: "Missing Location", description: "Placeholder description", images: [] },
         { x: 4871, y: 5940, label: "Weed Location #4", area: "Missing Location", description: "Placeholder description", images: [] },
-        { x: 6011, y: 3688, label: "Weed Location #5", area: "Missing Location", description: "Placeholder description", images: [] },
-        { x: 6481, y: 2566, label: "Weed Location #6", area: "Missing Location", description: "Placeholder description", images: [] },
+        { x: 6011, y: 3688, label: "Weed Location #5", area: "East of Quary", description: "Found by Drug Officer During routine search of State Borders", images: ["https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image3.png", "https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image4.png"] },
+        { x: 6481, y: 2566, label: "Weed Location #6", area: "Northern HumaneLabs Island", description: "Known spot by Drug Officers", images: ["https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image.png"] },
     ],
     meth: [
-        { x: 5707, y: 4998, label: "Meth Table #1", area: "Missing Location", description: "Placeholder description", images: [] },
+        { x: 5707, y: 4998, label: "Meth Table #1", area: "South Palmer-Taylor Power Station", description: "Found by Drug Officer during routine search of State Borders", images: ["https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image5.png"] },
         { x: 2619, y: 5285, label: "Meth Table #2", area: "Missing Location", description: "Placeholder description", images: [] },
     ],
     washing: [
         { x: 5391, y: 6397, label: "Washing Machine #1", area: "Missing Location", description: "Placeholder description", images: [] },
         { x: 5593, y: 6021, label: "Washing Machine #2", area: "Missing Location", description: "Placeholder description", images: [] },
         { x: 5599, y: 5711, label: "Washing Machine #3", area: "Missing Location", description: "Placeholder description", images: [] },
-        { x: 4770, y: 3162, label: "Washing Machine #4", area: "Missing Location", description: "Placeholder description", images: [] },
+        { x: 4770, y: 3162, label: "Washing Machine #4", area: "Sandy Shores Abandoned Motel", description: "Found by Drug Officer during a 10-80, uncommon spot", images: ["https://r2.fivemanage.com/WFJUnztU6NFDzltrV7jCl/image2.png"] },
     ]
 };
 
